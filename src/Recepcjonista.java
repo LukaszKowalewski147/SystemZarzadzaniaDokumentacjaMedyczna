00fc78a7100d18a79f1da7c5a2095f5420ccee1b
Łukasz Kowalewski <kowalewski.ukasz@gmail.com>
2025-01-15 00:41:24 +0100

    Recepcjonista base done.

--- src/Recepcjonista.java
+++ src/Recepcjonista.java
@@ -1,2 +1,14 @@
 public class Recepcjonista extends Pracownik {
+
+    private String numerStanowiska;
+
+    public Recepcjonista(String imię, String nazwisko, String numerTelefonu,
+                         String email, String godzinyPracy, String numerStanowiska) {
+        super(imię, nazwisko, numerTelefonu, email, godzinyPracy);
+        this.numerStanowiska = numerStanowiska;
+    }
+
+    public void rejestrujKonto() {
+
+    }
 }
